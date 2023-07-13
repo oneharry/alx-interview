4e8495f517d21eb261bb7c6696a79ee10ffa2e24
Oneharry <harryofficialchidi@yahoo.com>
2023-07-13 22:29:47 +0100

    implemented minimum operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Minimum operation """
+
+def minOperations(n: int) -> int:
+    if n == 1:
+        return 0
+
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return minOperations(i) + (n // i)
+
+    return n
