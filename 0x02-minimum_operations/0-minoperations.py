3574790465c5f31ec9c12e3ae23214f3f61a4aa4
oneharry <harryofficialchidi@yahoo.com>
2023-07-13 21:56:50 +0000

    fixed bug, removing recursion approach

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,11 +4,15 @@
 
 def minOperations(n: int) -> int:
     """Returns the minimun number of operation to reach n(H)"""
-    if n == 1:
+    if n <= 1:
         return 0
 
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return minOperations(i) + (n // i)
+    i = 2
+    num_operation = 0
 
-    return n
+    while i <= n:
+        while n % i == 0:
+            num_operation += i
+            n //= i
+        i += 1
+    return num_operation
