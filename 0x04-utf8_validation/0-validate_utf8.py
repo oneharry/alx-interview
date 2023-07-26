7f8f9ae1f1a5a0045c9473b04f55423b84cacbbb
oneharry <harryofficialchidi@yahoo.com>
2023-07-26 19:07:29 +0000

    utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,18 +6,7 @@ def validUTF8(data):
     """Implement calidate UTF-8
     return True if data is a valid UTF-8 encoding
     False otherwise"""
-    count = 0
     for x in data:
-        if count == 0:
-            if x >> 5 == 0b110:
-                count = 1
-            elif x >> 4 == 0b1110:
-                count = 2
-            elif x >> 0b011110:
-                count = 3
-            elif x >> 7 == 1:
-                return False
-        else:
-            if x >> 6 != 0b10:
-                return False
+        if x.bit_length() > 7:
+            return False
     return True
