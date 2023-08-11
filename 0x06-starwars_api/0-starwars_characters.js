4ba1502f60576c760d86341d7eed1f7e27f7f15b
oneharry <harryofficialchidi@yahoo.com>
2023-08-11 01:05:08 +0000

    fixed semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,9 +4,9 @@ const request = require('request');
 const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
 request(url, (err, res, body) => {
-  if (err) {
+  if (err){
     console.log('Error', err);
-    throw err
+    throw err;
   }
   else {
     const chars = JSON.parse(body).characters;
@@ -21,6 +21,6 @@ const printChars = (chars, num) => {
   request(chars[num], (err, res, body) => {
     if (err) throw err;
     console.log(JSON.parse(body).name);
-    printChars(chars, num + 1)
+    printChars(chars, num + 1);
   });
 };
