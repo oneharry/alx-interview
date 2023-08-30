177b26bee4606eac612a2823667c9c22f94f62cd
oneharry <harryofficialchidi@yahoo.com>
2023-08-30 23:54:12 +0000

    implemented island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,4 +4,20 @@
 
 def island_perimeter(grid):
     """ Implement island perimeter """
-    
+    if not grid:
+        return
+
+    rows = len(grid)
+    cols = len(grid[0])
+
+    perimeter = 0
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+    return perimeter
