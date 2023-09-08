d08005b51825dd43f68e6c0dc28991643414d919
Oneharry <harryofficialchidi@yahoo.com>
2023-09-08 01:28:07 +0100

    fixed bug

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,49 +1,38 @@
-#!/usr/bin/python3
-""" Prime game """
-
-
 def isWinner(x, nums):
-    """Determines the winner of the game"""
-    def is_prime(num):
-        if num <= 1:
-            return False
-        if num <= 3:
-            return True
-        if num % 2 == 0 or num % 3 == 0:
-            return False
-        i = 5
-        while i * i <= num:
-            if num % i == 0 or num % (i + 2) == 0:
-                return False
-            i += 6
-        return True
-
-    def find_primes_up_to_n(n):
-        primes = []
-        for i in range(2, n + 1):
-            if is_prime(i):
-                primes.append(i)
+    def sieve_erathosthenes(n):
+        is_prime = [True] * (n + 1)
+        is_prime[0] = is_prime[1] = False
+        p = 2
+        while p * p <= n:
+            if is_prime[p]:
+                for i in range(p * p, n + 1, p):
+                    is_prime[i] = False
+            p += 1
+        primes = [i for i in range(2, n + 1) if is_prime[i]]
         return primes
 
-    def play_round(n):
-        primes = find_primes_up_to_n(n)
-        maria_turn = True
-        while n > 0:
-            can_remove = False
-            for prime in primes:
-                if prime <= n:
-                    n -= prime
-                    can_remove = True
-                    break
-            if not can_remove:
-                break
-            maria_turn = not maria_turn
-        return "Maria" if maria_turn else "Ben"
+    def canWin(num, primes):
+        if num in primes:
+            return True
+        for prime in primes:
+            if num % prime == 0:
+                return True
+        return False
+
+    max_num = max(nums)
+    primes = sieve_erathosthenes(max_num)
+
+    dp = [0] * (max_num + 1)
+    dp[0] = 0
+    dp[1] = 0
+
+    for i in range(2, max_num + 1):
+        if canWin(i, primes):
+            dp[i] = 1
 
     winners = {"Maria": 0, "Ben": 0}
     for n in nums:
-        round_winner = play_round(n)
-        winners[round_winner] += 1
+        winners["Maria" if dp[n] == 1 else "Ben"] += 1
 
     if winners["Maria"] > winners["Ben"]:
         return "Maria"
